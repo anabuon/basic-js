048ba897f856b91cc609343113a4ae0acbd06abc
Ana  Buon <anabuon@icloud.com>
2021-03-26 13:25:18 +0300

    feat: add solution for transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error()
+  }
+  let result = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == '--discard-next' && arr[i + 1] !== undefined) {
+      i++
+    } else if (
+      arr[i] == '--discard-prev' &&
+      arr[i - 1] !== undefined &&
+      arr[i - 2] != '--discard-next'
+    ) {
+      result.pop()
+    } else if (arr[i] == '--double-next' && arr[i + 1] !== undefined) {
+      result.push(arr[i + 1])
+    } else if (
+      arr[i] == '--double-prev' &&
+      arr[i - 1] !== undefined &&
+      arr[i - 2] != '--discard-next'
+    ) {
+      result.push(arr[i - 1])
+    } else if (
+      arr[i] != '--double-prev' &&
+      arr[i] != '--double-next' &&
+      arr[i] != '--discard-prev' &&
+      arr[i] != '--discard-next'
+    ) {
+      result.push(arr[i])
+    }
+  }
+  return result
+}
